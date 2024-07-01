0d5ef7e687476770cb57d9ad7577a19079acc900
Sean Fitzgerald <sean@fitzgeralds.me>
2024-06-30 18:13:01 -0700

    Update prompt generation to summarize past 10k

--- heare/developer/prompt.py
+++ heare/developer/prompt.py
@@ -1,31 +1,40 @@
 import os
 
-def create_system_message(sandbox):
-    system_message = f"You are an AI assistant with access to a sandbox environment. The current contents of the sandbox are:\n"
-    
-    system_message += "<sandbox_contents>\n"
+from heare.developer.summarize import summarize_file
+
+
+def render_sandbox_content(sandbox, summarize):
     cwd = os.getcwd()
+
+    result = "<sandbox_contents>\n"
     for file_path in sandbox.list_sandbox():
         relative_path = os.path.relpath(file_path, cwd)
         try:
             file_content = sandbox.read_file(file_path)
-            total_tokens = estimate_token_count(file_content)
-            
-            if total_tokens > 500:  # Limit summary to about 500 tokens
-                words = file_content.split()
-                summary = ' '.join(words[:385])  # Approximately 500 tokens
-                system_message += f"<file path='{relative_path}' summarized='true'>\n"
-                system_message += f"{summary}...\n"
+            if summarize:  # Limit summary to about 500 tokens
+                summary = summarize_file(file_content)
+                result += f"<file path='{relative_path}' summarized='true'>\n"
+                result += f"{summary}...\n"
             else:
-                system_message += f"<file path='{relative_path}' summarized='false'>\n"
-                system_message += f"{file_content}\n"
+                result += f"<file path='{relative_path}' summarized='false'>\n"
+                result += f"{file_content}\n"
         except Exception as e:
-            system_message += f"<file path='{relative_path}' summarized='false'>\n"
-            system_message += f"Unable to read file: {str(e)}\n"
-        
-        system_message += "</file>\n"
-    system_message += "</sandbox_contents>\n"
+            result += f"<file path='{relative_path}' summarized='false'>\n"
+            result += f"Unable to read file: {str(e)}\n"
 
+        result += "</file>\n"
+    result += "</sandbox_contents>\n"
+    return result
+
+
+def create_system_message(sandbox, MAX_ESTIMATED_TOKENS=10_240):
+    system_message = f"You are an AI assistant with access to a sandbox environment. The current contents of the sandbox are:\n"
+    cwd = os.getcwd()
+    sandbox_content = render_sandbox_content(sandbox, False)
+    if estimate_token_count(sandbox_content):
+        sandbox_content = render_sandbox_content(sandbox, True)
+
+    system_message += sandbox_content
     system_message += "\nYou can read, write, and list files/directories, as well as execute some bash commands."
     
     return system_message
